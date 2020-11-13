98d210ccb09932976e77da565e7126911f88ba71
Andharikar9011 <48117912+Andharikar9011@users.noreply.github.com>
2020-11-13 22:09:03 +0530

    minor

--- assets/js/wp_intregate.js
+++ assets/js/wp_intregate.js
@@ -2,6 +2,7 @@ function send_data() {
     var form = new FormData(document.forms['ams'] )
     var msg = "Hi, I am "+form.get('name')+". I would like to contact you regarding my requirements about "+form.get('subject')+". Please mail me details at "+form.get('sender')+"."
     window.open(`https://wa.me/918329811873?text=${msg}`, '_blank');
+    location.reload()
     }  
  
     
