553288858a4f58555152ba74311c3f7eef91f529
Andharikar9011 <48117912+Andharikar9011@users.noreply.github.com>
2020-11-13 21:43:42 +0530

    integrate

--- assets/js/wp_intregate.js
+++ assets/js/wp_intregate.js
@@ -1,10 +1,8 @@
 function send_data() {
-    // alert("This is Working")
-    var keys=[];
-    var message=[];
-    const form = new FormData(document.forms['hairdo'])
-        var out_message = "Hi . This is "+form.get('name')+".%0D%0A I would like to make an appointment for "+form.getAll('services')+ ",%0D%0A On Date "+form.get('date') + " around "+form.get('time') +".%0D%0A This appointment is set by HairDo.";    // console.log(form.values())
-        // alert(form.values())
-        var win = window.open(`https://wa.me/918329811873?text=${out_message}`, '_blank');
+    // alert("This is working")
+    var form = new FormData(document.forms['ams'] )
+    var msg = "Hi, I am "+form.get('name')+". I would like to contact you regarding my requirements about "+form.get('subject')+". Please mail me details at "+forms.get('email');
+    var win = window.open(`https://wa.me/918329811873?text=${msg}`, '_blank');
     }  
  
+    
