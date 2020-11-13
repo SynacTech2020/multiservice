556a54604317e27abc90141ffbdc987a2d92bd5d
Andharikar9011 <48117912+Andharikar9011@users.noreply.github.com>
2020-11-13 22:10:34 +0530

    wp mobile input done

--- assets/js/wp_intregate.js
+++ assets/js/wp_intregate.js
@@ -1,7 +1,7 @@
 function send_data() {
     var form = new FormData(document.forms['ams'] )
     var msg = "Hi, I am "+form.get('name')+". I would like to contact you regarding my requirements about "+form.get('subject')+". Please mail me details at "+form.get('sender')+"."
-    window.open(`https://wa.me/918329811873?text=${msg}`, '_blank');
+    window.open(`https://wa.me/919011599027?text=${msg}`, '_blank');
     location.reload()
     }  
  
