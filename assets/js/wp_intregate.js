28c55c1e73637f5fcdeb8ccecf77f225537f02b3
Andharikar9011 <48117912+Andharikar9011@users.noreply.github.com>
2020-11-18 14:35:22 +0530

    changes

--- assets/js/wp_intregate.js
+++ assets/js/wp_intregate.js
@@ -1,3 +1,4 @@
+'use strict';
 // var product_arr=[];
 // sessionStorage.setItem("products",product_arr);
 
@@ -14,7 +15,6 @@ function add_product(param1) {
        var i = sessionStorage.length
         sessionStorage.setItem("products"+i,param1);
         console.log(sessionStorage)
-        alert('hi')
     }else{
         alert('This Product is already Added in Quotation List.')
     }
@@ -31,3 +31,24 @@ function search(param2) {
     return flag  
 }
     
+function send_quote(){
+    var quote_list='';
+    for (let index = 0; index < sessionStorage.length; index++) {
+        var p = sessionStorage.getItem('products'+index)
+        quote_list=quote_list+p+", "
+        }
+    var form = new FormData(document.forms['ams_q'] )
+    // alert (typeof(form.get('Additional')))
+    // console.log(typeof(form.get('Additional')))
+    // console.log(form.get('Additional'))
+    if(form.get('Additional')!==""){
+    var msg = "Hi, I am "+form.get('Name')+". I have requirements of "+quote_list+" also my additional requirements are "+form.get('Additional')+". You can mail me the details at "+form.get('Email')+" or Call me at "+form.get('Mobile')+"."
+    window.open(`https://wa.me/919011599027?text=${msg}`, '_blank');
+    }else{
+        var msg = "Hi, I am "+form.get('Name')+". I have requirements of "+quote_list+". You can mail me the details at "+form.get('Email')+" or Call me at "+form.get('Mobile')+"."
+        window.open(`https://wa.me/919011599027?text=${msg}`, '_blank');
+    }
+    
+    sessionStorage.clear()
+    location.replace('https://aradhanamultiservices.com/')
+}
